91c748027057ead23d1b09e98a4f530d3361f540
tqk2811 <tqk2811@gmail.com>
2025-09-27 10:32:37 +0700

    Singleton private

--- TqkLibrary.AegisubTemplateHelper/Singleton.cs
+++ TqkLibrary.AegisubTemplateHelper/Singleton.cs
@@ -4,8 +4,8 @@ namespace TqkLibrary.AegisubTemplateHelper
 {
     public static class Singleton
     {
-        public static string CurrentDir { get; set; } = new FileInfo(Assembly.GetCallingAssembly().Location).Directory!.FullName;
-        public static string AppDataDir { get; set; } = Path.Combine(CurrentDir, "AppData");
+        static string CurrentDir { get; } = new FileInfo(Assembly.GetCallingAssembly().Location).Directory!.FullName;
+        static string AppDataDir { get; } = Path.Combine(CurrentDir, "AppData");
         public static string AegisubDir { get; set; } = Path.Combine(AppDataDir, "Aegisub-3.4.2-portable");
     }
 }
