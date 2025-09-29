441ef7ecafcbb1023840ad40687ef98fa59f2940
tqk2811 <tqk2811@gmail.com>
2025-09-29 18:05:00 +0700

    fix script load wrong in folder

--- TqkLibrary.AegisubTemplateHelper/AppData/Aegisub-3.4.2-portable/automation/include/tqkextended.lua
+++ TqkLibrary.AegisubTemplateHelper/AppData/Aegisub-3.4.2-portable/automation/include/tqkextended.lua
@@ -1,4 +1,4 @@
-﻿local measure_multiline_width_height = require("tqkextended.measure_multiline_width_height")
+﻿local measure_multiline_width_height = require("tqkextended_measure_multiline_width_height")
 
 function tqk_extended_filter(meta, styles, subs)
     measure_multiline_width_height.measure_multiline_width_height_filter(meta, styles, subs)
