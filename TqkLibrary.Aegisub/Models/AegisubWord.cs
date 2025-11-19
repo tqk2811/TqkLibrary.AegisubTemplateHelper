7e4474df33797f1b975c460dc6e07f84343cc1d1
tqk2811 <tqk2811@gmail.com>
2025-11-19 20:05:09 +0700

    AegisubWord add ToString

--- TqkLibrary.Aegisub/Models/AegisubWord.cs
+++ TqkLibrary.Aegisub/Models/AegisubWord.cs
@@ -5,5 +5,9 @@ namespace TqkLibrary.Aegisub.Models
     public class AegisubWord : AegisubTime, IAegisubWord
     {
         public required string Word { get; set; }
+        public override string ToString()
+        {
+            return $"{Start:hh\\:mm\\:ss\\.fff} - {End:hh\\:mm\\:ss\\.fff}: {Word}";
+        }
     }
 }
