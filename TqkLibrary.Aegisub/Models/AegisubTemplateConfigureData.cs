6d93d7a374db8c415d350322b50363167dfe5eeb
tqk2811 <tqk2811@gmail.com>
2025-11-19 19:43:51 +0700

    remove HiddenFieldValues

--- TqkLibrary.Aegisub/Models/AegisubTemplateConfigureData.cs
+++ TqkLibrary.Aegisub/Models/AegisubTemplateConfigureData.cs
@@ -11,14 +11,10 @@ namespace TqkLibrary.Aegisub.Models
         [JsonConverter(typeof(AegisubTemplateDictionaryConverter))]
         public Dictionary<string, object> FieldValues { get; set; } = new();
 
-        [JsonConverter(typeof(AegisubTemplateDictionaryConverter))]
-        public Dictionary<string, object> HiddenFieldValues { get; set; } = new();
-
 
 
 
         static readonly Regex regex_FieldValues = new Regex("\\[([A-z0-9]+);([A-z0-9]+)\\]");
-        static readonly Regex regex_HiddenFieldValues = new Regex("\\[([A-z0-9]+);;([A-z0-9]+)\\]");
         public virtual async Task LoadFieldAsync()
         {
             var lines = await File.ReadAllLinesAsync(TemplateFilePath);
@@ -26,7 +22,6 @@ namespace TqkLibrary.Aegisub.Models
             List<string> fields = new List<string>();
 
             List<string> currentFields = new();
-            List<string> currentHiddenFields = new();
             foreach (var line in lines)
             {
                 MatchCollection matchCollection = regex_FieldValues.Matches(line);
@@ -42,31 +37,12 @@ namespace TqkLibrary.Aegisub.Models
                         FieldValues[name] = AegisubTemplateDictionaryConverter.CreateDefaultTypeHelper(type, defaultValue);
                     }
                 }
-
-                matchCollection = regex_HiddenFieldValues.Matches(line);
-                foreach (Match match in matchCollection)
-                {
-                    string name = match.Groups[1].Value;
-                    string defaultValue = match.Groups[2].Value;
-                    Type type = AegisubTemplateDictionaryConverter.GetTypeHelper(name);
-
-                    currentHiddenFields.Add(name);
-                    if (!HiddenFieldValues.ContainsKey(name))
-                    {
-                        HiddenFieldValues[name] = AegisubTemplateDictionaryConverter.CreateDefaultTypeHelper(type, defaultValue);
-                    }
-                }
             }
 
             List<string> exceptFields = FieldValues.Keys.Except(currentFields).ToList();
             foreach (var item in exceptFields)
             {
                 FieldValues.Remove(item);
-            }
-            List<string> exceptHiddenFields = HiddenFieldValues.Keys.Except(currentHiddenFields).ToList();
-            foreach (var item in exceptHiddenFields)
-            {
-                HiddenFieldValues.Remove(item);
             }
         }
 
@@ -101,27 +77,6 @@ namespace TqkLibrary.Aegisub.Models
                         }
 
                         line = Regex.Replace(line, $"\\[{Regex.Escape(item.Key)};([A-z0-9]+)\\]", replaced);
-                    }
-                    foreach (var item in HiddenFieldValues)
-                    {
-#if DEBUG
-                        var type = item.Value.GetType();
-#endif
-                        string replaced;
-                        if (item.Value is System.Drawing.Color color)
-                        {
-                            replaced = color.ToAssColor();
-                        }
-                        else if (item.Value is float f)
-                        {
-                            replaced = f.ToString("F1");
-                        }
-                        else
-                        {
-                            replaced = item.Value.ToString()!;
-                        }
-
-                        line = Regex.Replace(line, $"\\[{Regex.Escape(item.Key)};;([A-z0-9]+)\\]", replaced);
                     }
                     return line;
                 })
