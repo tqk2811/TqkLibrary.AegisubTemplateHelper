03c5af19f99d322f9b06a7a256e7c5d7dc8e1f66
tqk2811 <tqk2811@gmail.com>
2025-11-20 14:38:38 +0700

    SplitWords for text

--- TqkLibrary.Aegisub.TemplateHelper/AegisubTemplateExtensions.cs
+++ TqkLibrary.Aegisub.TemplateHelper/AegisubTemplateExtensions.cs
@@ -17,35 +17,69 @@ namespace TqkLibrary.Aegisub.TemplateHelper
                     yield return sentence;
                 }
             }
-
-            int wasTakeCount = 0;
-            for (int i = 0; i < sentence.Words.Count && wasTakeCount < sentence.Words.Count; i++)
+            if (sentence.Words.Any())
             {
-                if (i < wasTakeCount)
-                    continue;
-                var takewords = sentence.Words.Skip(wasTakeCount).Take(i - wasTakeCount + 1).ToList();
-                string text = string.Join(" ", takewords);
-                var size = fontMeasurer.MeasureString(text);
-                if (size.Width > maxWidth || i == sentence.Words.Count - 1)//`tràn` hoặc `cuối` hoặc `tràn với 1 word`
+                int wasTakeCount = 0;
+                for (int i = 0; i < sentence.Words.Count && wasTakeCount < sentence.Words.Count; i++)
                 {
-                    var words = sentence.Words.Skip(wasTakeCount);
-                    bool isFullyWithOneWord = (i == wasTakeCount);//tràn với 1 word
-                    if (size.Width > maxWidth)//tràn
-                        words = words.Take(Math.Max(i - wasTakeCount, 1));//`tràn với 1 word` -> i - wasTakeCount = 0
-
-                    var wordsList = words.ToList();
-                    yield return new AegisubSentence()
+                    if (i < wasTakeCount)
+                        continue;
+                    var testTakewords = sentence.Words.Skip(wasTakeCount).Take(i - wasTakeCount + 1).ToList();
+                    string text = string.Join(" ", testTakewords.Select(x => x.Word));
+                    var size = fontMeasurer.MeasureString(text);
+                    if (size.Width > maxWidth || i == sentence.Words.Count - 1)//`tràn` hoặc `cuối` hoặc `tràn với 1 word`
                     {
-                        Start = sentence.Words.First().Start,
-                        End = sentence.Words.Last().End,
-                        Words = wordsList.Clone().ToList(),
-                        Text = string.Join(" ", wordsList.Select(x => x.Word))
-                    };
-                    if (isFullyWithOneWord) wasTakeCount = i + 1;
-                    else wasTakeCount = i;
+                        var realTakeWords = sentence.Words.Skip(wasTakeCount);
+                        bool isFullyWithOneWord = (i == wasTakeCount);//tràn với 1 word
+                        if (size.Width > maxWidth)//tràn
+                            realTakeWords = realTakeWords.Take(Math.Max(i - wasTakeCount, 1));//`tràn với 1 word` -> i - wasTakeCount = 0
+
+                        var wordsList = realTakeWords.ToList();
+                        yield return new AegisubSentence()
+                        {
+                            Start = sentence.Start,
+                            End = sentence.End,
+                            Words = wordsList.Clone().ToList(),
+                            Text = string.Join(" ", wordsList.Select(x => x.Word))
+                        };
+                        if (isFullyWithOneWord) wasTakeCount = i + 1;
+                        else wasTakeCount = i;
+                    }
+                }
+            }
+            else
+            {
+                var words = sentence.Text.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                int wasTakeCount = 0;
+                for (int i = 0; i < words.Length && wasTakeCount < words.Length; i++)
+                {
+                    if (i < wasTakeCount)
+                        continue;
+                    var testTakewords = words.Skip(wasTakeCount).Take(i - wasTakeCount + 1).ToList();
+                    string text = string.Join(" ", testTakewords);
+                    var size = fontMeasurer.MeasureString(text);
+                    if (size.Width > maxWidth || i == words.Length - 1)//`tràn` hoặc `cuối` hoặc `tràn với 1 word`
+                    {
+                        var realTakeWords = words.Skip(wasTakeCount);
+                        bool isFullyWithOneWord = (i == wasTakeCount);//tràn với 1 word
+                        if (size.Width > maxWidth)//tràn
+                            realTakeWords = realTakeWords.Take(Math.Max(i - wasTakeCount, 1));//`tràn với 1 word` -> i - wasTakeCount = 0
+
+                        var wordsList = realTakeWords.ToList();
+                        yield return new AegisubSentence()
+                        {
+                            Start = sentence.Start,
+                            End = sentence.End,
+                            Words = [],
+                            Text = string.Join(" ", wordsList)
+                        };
+                        if (isFullyWithOneWord) wasTakeCount = i + 1;
+                        else wasTakeCount = i;
+                    }
                 }
             }
         }
+
 
         public static IEnumerable<IEnumerable<T>> SplitWords<T>(this IReadOnlyList<T> words, int line)
         {
