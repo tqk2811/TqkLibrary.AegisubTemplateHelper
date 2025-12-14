f83afa8209564d9aa8f97f1a2e124e669a971b74
tqk2811 <tqk2811@gmail.com>
2025-12-14 20:39:46 +0700

    AegisubTemplateConfigureFieldValue add clone

--- TqkLibrary.Aegisub/Models/AegisubTemplateConfigureFieldValue.cs
+++ TqkLibrary.Aegisub/Models/AegisubTemplateConfigureFieldValue.cs
@@ -10,8 +10,18 @@
             return new AegisubTemplateConfigureFieldValue<T>()
             {
                 Value = (T)this.Value,
-                MinValue = this.MinValue is not null? ((T)this.MinValue) : default(T),
+                MinValue = this.MinValue is not null ? ((T)this.MinValue) : default(T),
                 MaxValue = this.MaxValue is not null ? ((T)this.MaxValue) : default(T),
+            };
+        }
+
+        public AegisubTemplateConfigureFieldValue Clone()
+        {
+            return new AegisubTemplateConfigureFieldValue()
+            {
+                Value = this.Value,
+                MinValue = this.MinValue,
+                MaxValue = this.MaxValue,
             };
         }
     }
@@ -20,5 +30,15 @@
         public required T Value { get; set; }
         public T? MinValue { get; set; }
         public T? MaxValue { get; set; }
+
+        public AegisubTemplateConfigureFieldValue<T> Clone()
+        {
+            return new AegisubTemplateConfigureFieldValue<T>()
+            {
+                Value = this.Value,
+                MinValue = this.MinValue,
+                MaxValue = this.MaxValue,
+            };
+        }
     }
 }
