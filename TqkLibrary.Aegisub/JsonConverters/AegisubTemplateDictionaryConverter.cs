4da31da01e4ca637c21ab16a7624814539e95a3a
tqk2811 <tqk2811@gmail.com>
2025-12-15 10:43:34 +0700

    change AegisubTemplateDictionaryConverter

--- TqkLibrary.Aegisub/JsonConverters/AegisubTemplateDictionaryConverter.cs
+++ TqkLibrary.Aegisub/JsonConverters/AegisubTemplateDictionaryConverter.cs
@@ -5,31 +5,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TqkLibrary.Aegisub.Models;
 
 namespace TqkLibrary.Aegisub.JsonConverters
 {
-    public class AegisubTemplateDictionaryConverter : JsonConverter<Dictionary<string, object>>
+    public class AegisubTemplateDictionaryConverter : JsonConverter<Dictionary<string, AegisubTemplateConfigureFieldValue>>
     {
         public AegisubTemplateDictionaryConverter()
         {
         }
 
-        public override Dictionary<string, object> ReadJson(
+        public override Dictionary<string, AegisubTemplateConfigureFieldValue>? ReadJson(
             JsonReader reader,
             Type objectType,
-            Dictionary<string, object>? existingValue,
+            Dictionary<string, AegisubTemplateConfigureFieldValue>? existingValue,
             bool hasExistingValue,
             JsonSerializer serializer
             )
         {
-            var result = new Dictionary<string, object>();
-            var jObject = JObject.Load(reader);
-
-            foreach (var prop in jObject.Properties())
+            if (reader.TokenType == JsonToken.Null)
             {
-                Type type = GetTypeHelper(prop.Name);
-                var value = prop.Value.ToObject(type, serializer)!;
-                result[prop.Name] = value;
+                return null;
+            }
+
+            var jsonObject = JObject.Load(reader);
+            var result = existingValue ?? new Dictionary<string, AegisubTemplateConfigureFieldValue>();
+
+            foreach (var property in jsonObject.Properties())
+            {
+                string key = property.Name;
+                JObject fieldObject = (JObject)property.Value;
+                Type valueType = GetTypeHelper(key);
+                var innerSerializer = new JsonSerializer
+                {
+                    ContractResolver = serializer.ContractResolver,
+                    TypeNameHandling = serializer.TypeNameHandling,
+                    Converters = { new ValueTypeConverter(valueType) }
+                };
+                var fieldValue = fieldObject.ToObject<AegisubTemplateConfigureFieldValue>(innerSerializer);
+                if (fieldValue != null)
+                {
+                    result[key] = fieldValue;
+                }
             }
 
             return result;
@@ -37,18 +54,31 @@ namespace TqkLibrary.Aegisub.JsonConverters
 
         public override void WriteJson(
             JsonWriter writer,
-            Dictionary<string, object>? value,
+            Dictionary<string, AegisubTemplateConfigureFieldValue>? value,
             JsonSerializer serializer
             )
         {
-            if (value is not null)
+            if (value is null)
+            {
+                writer.WriteNull();
+                return;
+            }
+            else
             {
                 writer.WriteStartObject();
 
                 foreach (var kvp in value)
                 {
-                    writer.WritePropertyName(kvp.Key.ToString());
-                    serializer.Serialize(writer, kvp.Value);
+                    writer.WritePropertyName(kvp.Key);
+                    Type valueType = GetTypeHelper(kvp.Key);
+                    var innerSerializer = new JsonSerializer
+                    {
+                        Formatting = serializer.Formatting,
+                        ContractResolver = serializer.ContractResolver,
+                        TypeNameHandling = serializer.TypeNameHandling,
+                        Converters = { new ValueTypeConverter(valueType) }
+                    };
+                    innerSerializer.Serialize(writer, kvp.Value);
                 }
 
                 writer.WriteEndObject();
@@ -66,10 +96,6 @@ namespace TqkLibrary.Aegisub.JsonConverters
             {
                 return typeof(int);
             }
-            //if (name.EndsWith("Int8"))
-            //{
-            //    return typeof(byte);
-            //}
             if (name.EndsWith("Float"))
             {
                 return typeof(float);
@@ -102,6 +128,68 @@ namespace TqkLibrary.Aegisub.JsonConverters
                 return float.Parse(defaultValue);
             }
             return defaultValue;
+        }
+
+
+
+        public class ValueTypeConverter : JsonConverter
+        {
+            private readonly Type _targetType;
+
+            public ValueTypeConverter(Type targetType)
+            {
+                _targetType = targetType;
+            }
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(object) || objectType == typeof(AegisubTemplateConfigureFieldValue);
+            }
+            public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+            {
+                if (value == null)
+                {
+                    writer.WriteNull();
+                    return;
+                }
+
+                if (value.GetType() == _targetType)
+                {
+                    serializer.Serialize(writer, value);
+                }
+                else
+                {
+                    try
+                    {
+                        object convertedValue = Convert.ChangeType(value, _targetType);
+                        serializer.Serialize(writer, convertedValue);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        serializer.Serialize(writer, value);
+                    }
+                    catch (FormatException)
+                    {
+                        serializer.Serialize(writer, value);
+                    }
+                }
+            }
+            public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+            {
+                if (reader.TokenType == JsonToken.Null)
+                {
+                    return null;
+                }
+
+                try
+                {
+                    JToken token = JToken.Load(reader);
+                    return token.ToObject(_targetType, serializer);
+                }
+                catch (Exception ex)
+                {
+                    throw new JsonSerializationException($"Không thể deserialize giá trị về kiểu {_targetType.FullName}.", ex);
+                }
+            }
         }
     }
 }
